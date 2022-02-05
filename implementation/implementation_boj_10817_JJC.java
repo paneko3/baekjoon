8a5fcbec079ab139635ec458309efb77c2f3bf79
paneko3 <34058812+paneko3@users.noreply.github.com>
2022-02-06 08:37:15 +0900

    Add files via upload

---
+++ implementation/implementation_boj_10817_JJC.java
@@ -0,0 +1,20 @@
+// implementation_boj_10817_JJC
+
+import java.util.*;
+
+public class implementation_boj_10817_JJC {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[3];
+        for(int i=0;i<3;i++){
+            arr[i]=sc.nextInt();
+        }
+        Arrays.sort(arr);
+        System.out.println(arr[1]);
+
+        sc.close();
+    }
+
+
+}
