3332a5091e1601a3ddcfa4ae2421d3c700a84f60
paneko3 <34058812+paneko3@users.noreply.github.com>
2022-06-25 07:45:39 +0900

    BOJ_15666_N과 M (12)

---
+++ SILVER/S2/BOJ_15666.java
@@ -0,0 +1,57 @@
+// BOJ_15666_N과 M (12)
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class BOJ_15666 {
+    static int N,M;
+    static int[] input;
+    static int[] order;
+    static boolean[] selected;
+    static Set<String> set;
+    static StringBuilder sb;
+    public static void main(String[] args) throws IOException {
+        /* ==========input========== */
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st= new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        input = new int[N];
+        st= new StringTokenizer(br.readLine());
+        for(int i=0;i<N;i++){
+            input[i]=Integer.parseInt(st.nextToken());
+        }
+        /* ==========sol========== */
+        Arrays.sort(input);
+        selected=new boolean[N];
+        order=new int[M];
+        set=new HashSet<>();
+        sb = new StringBuilder();
+        recur(0,0);
+        /* ==========output========== */
+        System.out.println(sb);
+        br.close();
+    }
+    static void recur(int count,int start){
+        if(count==M){
+            String str = "";
+            for(int i=0;i<M;i++){
+                str+=order[i]+" ";
+            }
+            if(!set.contains(str)){
+                set.add(str);
+                sb.append(str).append("\n");
+            }
+            return;
+        }
+        for(int i=start;i<N;i++){
+            order[count]=input[i];
+            recur(count+1,i);
+        }
+    }
+}
