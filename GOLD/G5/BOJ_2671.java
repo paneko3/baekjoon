d582bf2a0a51ee98324c44b51f035cb4f2a65bf9
Jaecheol Jeong <paneko3@gmail.com>
2022-07-26 23:01:44 +0900

    BOJ_2475_검증수

--- GOLD/G5/BOJ_2671.java
+++ GOLD/G5/BOJ_2671.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class BOJ_2671 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         /* ==========input========== */
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str=br.readLine();
