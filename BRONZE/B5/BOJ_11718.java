09b28b774b74861f39c550e65b9a5dac5f8ce57d
paneko3 <34058812+paneko3@users.noreply.github.com>
2022-08-16 23:43:38 +0900

    BOJ_11718_그대로 출력하기

---
+++ BRONZE/B5/BOJ_11718.java
@@ -0,0 +1,19 @@
+// BOJ_11718_그대로 출력하기
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_11718 {
+    public static void main(String[] args) throws IOException {
+        /* ==========input========== */
+        /* ==========sol========== */
+        /* ==========output========== */
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str="";
+        while((str=br.readLine())!=null){
+            System.out.println(str);
+        }
+        br.close();
+    }
+}
