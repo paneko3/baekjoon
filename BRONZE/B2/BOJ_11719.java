fbfd24e96cf834f572f666a729796a5bd7c2c389
paneko3 <34058812+paneko3@users.noreply.github.com>
2022-08-19 22:25:08 +0900

    BOJ_11719_그대로 출력하기 2

---
+++ BRONZE/B2/BOJ_11719.java
@@ -0,0 +1,19 @@
+// BOJ_11719_그대로 출력하기 2
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_11719 {
+    public static void main(String[] args) throws IOException {
+        /* ==========input========== */
+        /* ==========sol========== */
+        /* ==========output========== */
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str;
+        while ((str=br.readLine())!=null){
+            System.out.println(str);
+        }
+        br.close();
+    }
+}
