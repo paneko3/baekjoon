c46b7f03df20e069a73b8be5d892c7ce588b4a02
paneko3 <34058812+paneko3@users.noreply.github.com>
2022-08-25 22:33:50 +0900

    BOJ_7287_등록

---
+++ BRONZE/B5/BOJ_7287.java
@@ -0,0 +1,11 @@
+// BOJ_7287_등록
+
+public class BOJ_7287 {
+    public static void main(String[] args) {
+        /* ==========input========== */
+        /* ==========sol========== */
+        /* ==========output========== */
+        System.out.println(446);
+        System.out.println("paneko3");
+    }
+}
