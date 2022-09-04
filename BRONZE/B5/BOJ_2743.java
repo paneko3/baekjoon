80dae96a2b58ed8caedb050f31dfe78cd49e5676
paneko3 <34058812+paneko3@users.noreply.github.com>
2022-09-04 22:10:49 +0900

    BOJ_2743_단어 길이 재기

---
+++ BRONZE/B5/BOJ_2743.java
@@ -0,0 +1,16 @@
+// BOJ_2743_단어 길이 재기
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_2743 {
+    public static void main(String[] args) throws IOException {
+        /* ==========input========== */
+        /* ==========sol========== */
+        /* ==========output========== */
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine().length());
+        br.close();
+    }
+}
