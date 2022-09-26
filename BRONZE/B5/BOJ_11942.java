cd47d592cb073d40bf660a0f14e72571b9183105
JaeCheol <34058812+paneko3@users.noreply.github.com>
2022-09-26 22:22:31 +0900

    BOJ_11942_고려대는 사랑입니다

---
+++ BRONZE/B5/BOJ_11942.java
@@ -0,0 +1,10 @@
+// BOJ_11942_고려대는 사랑입니다
+
+public class BOJ_11942 {
+    public static void main(String[] args)  {
+        /* ==========input========== */
+        /* ==========sol========== */
+        /* ==========output========== */
+        System.out.println("고려대학교");
+    }
+}
