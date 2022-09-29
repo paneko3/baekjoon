130504dafada5655a1aaefcc13c2c7230efb0a5c
JaeCheol <34058812+paneko3@users.noreply.github.com>
2022-09-29 21:28:49 +0900

    BOJ_5337_웰컴

---
+++ BRONZE/B5/BOJ_5337.java
@@ -0,0 +1,12 @@
+// BOJ_5337_웰컴
+
+public class BOJ_5337 {
+    public static void main(String[] args) {
+        /* ==========input========== */
+        /* ==========sol========== */
+        /* ==========output========== */
+        System.out.println(".  .   .\n" +
+                "|  | _ | _. _ ._ _  _\n" +
+                "|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
