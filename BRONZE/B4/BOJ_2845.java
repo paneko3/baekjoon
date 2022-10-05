656a059543c048e5efa8c3aaeb6fbb5b91f8ad6f
JaeCheol <34058812+paneko3@users.noreply.github.com>
2022-10-05 23:20:30 +0900

    BOJ_2845_파티가 끝나고 난 뒤

---
+++ BRONZE/B4/BOJ_2845.java
@@ -0,0 +1,23 @@
+// BOJ_2845_파티가 끝나고 난 뒤
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_2845 {
+    public static void main(String[] args) throws IOException {
+        /* ==========input========== */
+        /* ==========sol========== */
+        /* ==========output========== */
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int L=Integer.parseInt(st.nextToken());
+        int P=Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<5;i++){
+            System.out.print(Integer.parseInt(st.nextToken())-L*P+" ");
+        }
+        br.close();
+    }
+}
