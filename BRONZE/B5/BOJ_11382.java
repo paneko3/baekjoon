e646eeed0c5cff10119eb5a63126b8658382827b
JaeCheol <34058812+paneko3@users.noreply.github.com>
2022-11-13 23:15:36 +0900

    BOJ_11382 꼬마 정민

--- BRONZE/B5/BOJ_11382.java
+++ BRONZE/B5/BOJ_11382.java
@@ -4,16 +4,16 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		/* ==========input========== */
-		/* ==========sol========== */
-		/* ==========output========== */
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		long A = Long.parseLong(st.nextToken());
-	    long B = Long.parseLong(st.nextToken());
+    public static void main(String[] args) throws IOException {
+        /* ==========input========== */
+        /* ==========sol========== */
+        /* ==========output========== */
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
         long C = Long.parseLong(st.nextToken());
-		System.out.println(A+B+C);
-		br.close();
-	}
+        System.out.println(A+B+C);
+        br.close();
+    }
 }
