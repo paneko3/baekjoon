be21a248b68c6fd06704019d126f5a23989a89f7
JaeCheol <34058812+paneko3@users.noreply.github.com>
2022-12-11 21:21:38 +0900

    BOJ_1057_토너먼트

---
+++ SILVER/S3/BOJ_1057.java
@@ -0,0 +1,27 @@
+// BOJ_1057_토너먼트
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_1057 {
+    public static void main(String[] args) throws IOException {
+        /* ==========input========== */
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        /* ==========sol========== */
+        int result =0;
+        while (a!=b){
+            a = a/2 +a%2;
+            b = b/2 +b%2;
+            result++;
+        }
+        /* ==========output========== */
+        System.out.println(result);
+        br.close();
+    }
+}
