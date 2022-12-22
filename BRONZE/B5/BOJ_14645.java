63070294ba337cab5f6807ae9b7b5e3d7f1e8e13
JaeCheol <34058812+paneko3@users.noreply.github.com>
2022-12-22 22:20:25 +0900

    BOJ_14645_와이버스 부릉부릉

---
+++ BRONZE/B5/BOJ_14645.java
@@ -0,0 +1,10 @@
+// BOJ_14645_와이버스 부릉부릉
+
+public class BOJ_14645 {
+    public static void main(String[] args) {
+        /* ==========input========== */
+        /* ==========sol========== */
+        /* ==========output========== */
+        System.out.println("비와이");
+    }
+}
