df32c536c9224c795de82f994b912d10e8b505be
JaeCheol <34058812+paneko3@users.noreply.github.com>
2023-01-23 11:26:41 +0900

    BOJ_15962_새로운 시작

---
+++ BRONZE/B5/BOJ_15962.java
@@ -0,0 +1,10 @@
+// BOJ_15962_새로운 시작
+
+public class BOJ_15962 {
+    public static void main(String[] args)  {
+        /* ==========input========== */
+        /* ==========sol========== */
+        /* ==========output========== */
+        System.out.println("파이팅!!");
+    }
+}
