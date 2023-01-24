a5e0679bd1be14b0ef499d4d5858a806220d67b7
JaeCheol <34058812+paneko3@users.noreply.github.com>
2023-01-24 09:20:11 +0900

    BOJ_15733_나는 누구인가

---
+++ BRONZE/B5/BOJ_15733.java
@@ -0,0 +1,10 @@
+// BOJ_15733_나는 누구인가
+
+public class BOJ_15733 {
+    public static void main(String[] args)  {
+        /* ==========input========== */
+        /* ==========sol========== */
+        /* ==========output========== */
+        System.out.println("I'm Sexy");
+    }
+}
