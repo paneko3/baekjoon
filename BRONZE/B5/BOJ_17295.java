3d2728550fc250d3968a2b90ce82b4265d23b94f
JaeCheol <34058812+paneko3@users.noreply.github.com>
2023-04-10 21:06:41 +0900

    BOJ_17295_엔드게임 스포일러

---
+++ BRONZE/B5/BOJ_17295.java
@@ -0,0 +1,16 @@
+// BOJ_17295_엔드게임 스포일러
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        /* ==========input========== */
+        /* ==========sol========== */
+        /* ==========output========== */
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Avengers: Endgame");
+        br.close();
+    }
+}
