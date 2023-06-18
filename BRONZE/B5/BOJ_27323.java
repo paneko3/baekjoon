29920f0121f082bd4a58618ac66d178d0224ede1
JaeCheol <34058812+paneko3@users.noreply.github.com>
2023-06-18 20:38:19 +0900

    BOJ_27323_직사각형

---
+++ BRONZE/B5/BOJ_27323.java
@@ -0,0 +1,16 @@
+// BOJ_27323_직사각형
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_27323 {
+    public static void main(String[] args) throws IOException {
+        /* ==========input========== */
+        /* ==========sol========== */
+        /* ==========output========== */
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(Integer.parseInt(br.readLine())*Integer.parseInt(br.readLine()));
+        br.close();
+    }
+}
