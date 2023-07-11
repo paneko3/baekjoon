deac1bf5c70d6302d08ed9d069ecc2cd92e39190
JaeCheol <34058812+paneko3@users.noreply.github.com>
2023-07-11 21:32:42 +0900

    BOJ_24263_알고리즘 수업 - 알고리즘의 수행 시간 2

---
+++ BRONZE/B4/BOJ_24263.java
@@ -0,0 +1,18 @@
+// BOJ_24263_알고리즘 수업 - 알고리즘의 수행 시간 2
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_24263 {
+    public static void main(String[] args) throws IOException {
+        /* ==========input========== */
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        /* ==========sol========== */
+        /* ==========output========== */
+        System.out.println(N+"\n1");
+        br.close();
+    }
+}
