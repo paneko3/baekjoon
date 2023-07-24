fef7c5ab3d975e5191c5fbfd09eb08efa9679b3f
JaeCheol <34058812+paneko3@users.noreply.github.com>
2023-07-24 20:15:26 +0900

    BOJ_2947_나무 조각

--- BRONZE/B1/BOJ_2947.java
+++ BRONZE/B1/BOJ_2947.java
@@ -16,29 +16,18 @@ public class BOJ_2947 {
         }
         /* ==========sol========== */
         /* ==========output========== */
-        while (true){
-            boolean flag = false;
-            for(int i=0;i<5;i++){
-                if(arr[i]!=i+1){
-                    flag = true;
-                    break;
+        for(int i=0;i<5;i++){
+            for(int j=0;j<4;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    for(int n : arr){
+                        System.out.print(n+" ");
+                    }
+                    System.out.println();
                 }
             }
-            if(!flag){
-                break;
-            }
-            for(int i=0;i<4;i++){
-                if(arr[i]>arr[i+1]){
-                    int temp = arr[i];
-                    arr[i]=arr[i+1];
-                    arr[i+1]=temp;
-                    break;
-                }
-            }
-            for(int n : arr){
-                System.out.print(n + " ");
-            }
-            System.out.println();
         }
         br.close();
     }
