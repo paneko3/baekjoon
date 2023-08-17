ea118f02901186b176b626a23e49633dba9c22aa
JaeCheol <34058812+paneko3@users.noreply.github.com>
2023-08-17 22:15:02 +0900

    BOJ_24723_녹색거탑

---
+++ BRONZE/B4/BOJ_24723.java
@@ -0,0 +1,16 @@
+// BOJ_24723_녹색거탑
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_24723 {
+    public static void main(String[] args) throws IOException {
+        /* ==========input========== */
+        /* ==========sol========== */
+        /* ==========output========== */
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println((int)(Math.pow(2,Integer.parseInt(br.readLine()))));
+        br.close();
+    }
+}
