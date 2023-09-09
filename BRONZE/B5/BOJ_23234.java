52e2bb1bff68e67295657812b8920ba95b491e82
JaeCheol <34058812+paneko3@users.noreply.github.com>
2023-09-09 23:00:07 +0900

    BOJ_23234_The World Responds

---
+++ BRONZE/B5/BOJ_23234.java
@@ -0,0 +1,10 @@
+// BOJ_23234_The World Responds
+
+public class BOJ_23234 {
+    public static void main(String[] args) {
+        /* ==========input========== */
+        /* ==========sol========== */
+        /* ==========output========== */
+        System.out.println("The world says hello!");
+    }
+}
