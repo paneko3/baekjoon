2467a3bc3f08316ce43394969c96c4a09688db0c
JaeCheol <34058812+paneko3@users.noreply.github.com>
2023-12-18 22:30:46 +0900

    BOJ_14935_FA

---
+++ BRONZE/B4/BOJ_14935.java
@@ -0,0 +1,10 @@
+// BOJ_14935_FA
+
+public class BOJ_14935 {
+    public static void main(String[] args) {
+        /* ==========input========== */
+        /* ==========sol========== */
+        /* ==========output========== */
+        System.out.println("FA");
+    }
+}
