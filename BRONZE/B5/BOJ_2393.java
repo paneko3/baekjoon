5dda6df2998271f0baae86eb4f5f76d1be220159
JaeCheol <34058812+paneko3@users.noreply.github.com>
2023-12-21 23:03:42 +0900

    BOJ_2393_Rook

---
+++ BRONZE/B5/BOJ_2393.java
@@ -0,0 +1,26 @@
+// BOJ_2393_Rook
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_2393 {
+    public static void main(String[] args) {
+        /* ==========input========== */
+        /* ==========sol========== */
+        /* ==========output========== */
+        System.out.println("  ___  ___  ___\n" +
+                "  | |__| |__| |\n" +
+                "  |           |\n" +
+                "   \\_________/\n" +
+                "    \\_______/\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |_____|\n" +
+                "  __/       \\__\n" +
+                " /             \\\n" +
+                "/_______________\\");
+    }
+}
