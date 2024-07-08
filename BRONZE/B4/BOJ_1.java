4c31bbdd52380457c48df3e6a932488449065af3
JaeCheol <34058812+paneko3@users.noreply.github.com>
2024-07-08 21:34:08 +0900

    BOJ_29790_임스의 메이플컵

---
+++ BRONZE/B4/BOJ_1.java
@@ -0,0 +1,18 @@
+// BOJ__
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_1 {
+    public static void main(String[] args) throws IOException {
+        /* ==========input========== */
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        /* ==========sol========== */
+        /* ==========output========== */
+        br.close();
+    }
+}
