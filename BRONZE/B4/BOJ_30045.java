29b7f8e2a781180b618fb5a41e5008fcac955b48
JaeCheol <34058812+paneko3@users.noreply.github.com>
2024-12-04 20:14:51 +0900

    BOJ_30045_ZOAC 6

---
+++ BRONZE/B4/BOJ_30045.java
@@ -0,0 +1,14 @@
+// BOJ__
+
+import java.io.*;
+
+public class BOJ_30045 {
+	public static void main(String[] args) throws IOException {
+		/* ==========input========== */
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		/* ==========sol========== */
+		/* ==========output========== */
+		br.close();
+	}
+}
