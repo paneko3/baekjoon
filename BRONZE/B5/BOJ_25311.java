f72bc36d0f3f7d2b91c693a6f37481f224355722
JaeCheol <34058812+paneko3@users.noreply.github.com>
2024-12-21 15:16:39 +0800

    BOJ_25311_UCPC에서 가장 쉬운 문제 번호는?

---
+++ BRONZE/B5/BOJ_25311.java
@@ -0,0 +1,17 @@
+// BOJ_25311_UCPC에서 가장 쉬운 문제 번호는?
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_25311 {
+    public static void main(String[] args) throws IOException {
+        /* ==========input========== */
+        /* ==========sol========== */
+        /* ==========output========== */
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        System.out.println("A");
+        br.close();
+    }
+}
