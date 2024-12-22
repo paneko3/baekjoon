83461f4c0f4c50e8f850a74f55a20c739f5c7878
JaeCheol <34058812+paneko3@users.noreply.github.com>
2024-12-22 10:47:20 +0800

    BOJ_10189_Hook

---
+++ BRONZE/B5/BOJ_10189.java
@@ -0,0 +1,17 @@
+// BOJ_10189_Hook
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_10189 {
+    public static void main(String[] args) throws IOException {
+        /* ==========input========== */
+        /* ==========sol========== */
+        /* ==========output========== */
+        System.out.println("#  # #### #### #  #");
+        System.out.println("#### #  # #  # # #");
+        System.out.println("#### #  # #  # # #");
+        System.out.println("#  # #### #### #  #");
+    }
+}
