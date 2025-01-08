eacb035fea9dba981577305011951ce7c80c5b88
JaeCheol <34058812+paneko3@users.noreply.github.com>
2025-01-08 20:38:56 +0800

    BOJ_23530_Not A + B

---
+++ BRONZE/B3/BOJ_23530.java
@@ -0,0 +1,22 @@
+// BOJ_23530_Not A + B
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ_23530 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int t = Integer.parseInt(st.nextToken());
+		for(int i=0;i<t;i++) {
+			/* ==========input========== */
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			/* ==========sol========== */
+			/* ==========output========== */
+			System.out.println(1);
+		}
+		br.close();
+	}
+}
