66d24b441adcd44f2cf1daa342614060980b7348
JaeCheol <34058812+paneko3@users.noreply.github.com>
2025-03-13 19:14:32 +0800

    BOJ_3041_N-퍼즐

---
+++ BRONZE/B1/BOJ_3041.java
@@ -0,0 +1,29 @@
+// BOJ_3041_N-퍼즐 
+
+import java.io.*;
+
+public class BOJ_3041 {
+	static char[][] arr = {{'A','B','C','D'},{'E','F','G','H'},{'I','J','K','L'},{'M','N','O','.'}};
+	public static void main(String[] args) throws IOException {
+		/* ==========input========== */
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int result = 0;
+		/* ==========sol========== */
+		for(int i=0;i<4;i++) {
+			String str = br.readLine();
+			for(int j=0;j<4;j++) {
+				char c = str.charAt(j);
+				for(int a=0;a<4;a++) {
+					for(int b=0;b<4;b++) {
+						if(c!='.'&&c==arr[a][b]) {
+							result += Math.abs(i-a)+Math.abs(j-b);
+						}
+					}
+				}
+			}
+		}
+		/* ==========output========== */
+		System.out.println(result);
+		br.close();
+	}
+}
