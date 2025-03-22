e97fe1850adca7f1b1a9b31546029c7ca39125d8
JaeCheol <34058812+paneko3@users.noreply.github.com>
2025-03-22 17:42:33 +0900

    BOJ_32458_Just Round Down

---
+++ BRONZE/B5/BOJ_32458.java
@@ -0,0 +1,15 @@
+// BOJ_32458_Just Round Down
+
+import java.io.*;
+
+public class BOJ_32458 {
+	public static void main(String[] args) throws IOException {
+		/* ==========input========== */
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        double N = Double.parseDouble(br.readLine());
+        /* ==========sol========== */
+        /* ==========output========== */
+        System.out.print((int)N);
+		br.close();
+	}
+}
