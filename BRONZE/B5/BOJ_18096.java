fa57bb790cfd3b83682df2e65392034e8dab9a77
JaeCheol <34058812+paneko3@users.noreply.github.com>
2025-05-07 21:02:24 +0900

    BOJ_18096_Арифметическая магия

---
+++ BRONZE/B5/BOJ_18096.java
@@ -0,0 +1,15 @@
+// BOJ_18096_Арифметическая магия
+
+import java.io.*;
+
+public class BOJ_18096 {
+	public static void main(String[] args) throws IOException {
+		/* ==========input========== */
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        /* ==========sol========== */
+        /* ==========output========== */
+        System.out.println(1);
+		br.close();
+	}
+}
