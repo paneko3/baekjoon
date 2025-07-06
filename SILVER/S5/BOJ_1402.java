d7f7ca782dc170be72b11992a5c75fd5e3aa8a60
JaeCheol <34058812+paneko3@users.noreply.github.com>
2025-07-06 20:47:35 +0900

    BOJ_1402_아무래도이문제는A번난이도인것같다

---
+++ SILVER/S5/BOJ_1402.java
@@ -0,0 +1,15 @@
+// BOJ_1402_아무래도이문제는A번난이도인것같다
+
+import java.util.*;
+
+public class BOJ_1402 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            sc.nextLong(); // A
+            sc.nextLong(); // B
+            System.out.println("yes");
+        }
+    }
+}
