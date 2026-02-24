19d74e5d2d07966bef1b9e0ab985a958d5d72769
JaeCheol <34058812+paneko3@users.noreply.github.com>
2026-02-24 20:33:09 +0900

    BOJ_3765_Celebrity jeopardy

---
+++ BRONZE/B4/BOJ_3765.java
@@ -0,0 +1,19 @@
+// BOJ_3765_Celebrity jeopardy
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ_3765 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s;
+        for (; (s = br.readLine()) != null; ) {
+            /* ==========input========== */
+            /* ==========sol========== */
+            String result = s;
+            /* ==========output========== */
+            System.out.println(result);
+        }
+        br.close();
+    }
+}
